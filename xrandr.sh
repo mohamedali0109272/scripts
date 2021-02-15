68b39f9b67a63f4f50873e614c609a0ca7373e42
mohamedali0109272 <mohamedali0109272@gmail.com>
2021-02-15 16:48:02 +0200

    r

---
+++ xrandr.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output  LVDS1 --scale 1.4055636896046853x1.40625 --panning 1920x1080
---
+++ xwinwrap.sh
@@ -0,0 +1,2 @@
+nice xwinwrap -b -s -fs -st -sp -nf -ov -fdt -- mpv -wid WID
+/Lucifer1/Lucifer.S01E01.480p.x264.mkv
