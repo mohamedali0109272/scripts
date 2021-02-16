46032f920e73227a2a1396637861476b63c914ad
mohamedali0109272 <mohamedali0109272@gmail.com>
2021-02-16 02:05:23 +0200

    do

---
+++ xwinwrap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xwinwrap -b -s -fs -st -sp -nf -ov -fdt -- mpv -wid WID --really-quiet --framedrop=vo --no-audio --panscan="1.0" ~/Renai\
+Circulation「恋愛サーキュレーション」歌ってみた【＊なみりん】-uKxyLmbOc0Q.mkv
